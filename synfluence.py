4143c4ef8d67a5b33fd9ac72be68fbcfd0dd84b6
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-01-13 18:11:40 -0800

    start synaptic influence script

---
+++ synfluence.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# load data
+base = 'C:\\Users\\geoff\\NEURONoutput\\'
+folder = 'lockedSyn\\'
+dataPath = base + folder
+
+condition = ['None', 'E', 'I', 'EI']
+
+VmTreeDF, iCaTreeDF = [], []
+for c in condition:
+    VmTreeDF.append(pd.read_hdf(dataPath+c+'\\treeRecData.h5', 'Vm'))
+    iCaTreeDF.append(pd.read_hdf(dataPath+c+'\\treeRecData.h5', 'iCa'))
+
+manipSynsDF = pd.read_csv(dataPath+'manipulatedSyns.csv')
+locationsDF = pd.read_csv(dataPath+'treeRecLocations.csv')
+
+# data dimensions
+# keep in mind that these have likely been downsampled
+# ran at 10kHz, downsampled by factor of 10
+trials = VmTreeDF[0].columns.levels[0].values
+directions = VmTreeDF[0].columns.levels[1].values
+tsteps, nrecs = VmTreeDF[0].shape[0], locationsDF.shape[0]
+numTrials, numDirs = len(trials), len(directions)
+print('num trials:', numTrials, ', directions:', directions)
+print('num recs:', nrecs, ', time steps:', tsteps)
+dendSegs = int(VmTreeDF[0].columns.size/numTrials/numDirs/350)
+
+
+def grabProxRecs(maxDist=50):
+    '''
+    Indices and cable distances for recs proximal to the manipulated synapses.
+    Locations may be used to roughly determine which side of the target synapse
+    each recording is on (bi-directional lineplot).
+    '''
+    distBetwRecs = pd.read_csv(dataPath+'distBetwRecs.csv')
+    proxDists = {}
+    proxLocs = {}
+    for dend in manipSynsDF['dendNum']:
+        mid = str(int(dend*dendSegs + dendSegs/2))
+        proxDists[dend] = distBetwRecs[mid][distBetwRecs[mid] < maxDist]
+        proxLocs[dend] = locationsDF.loc[proxDists[dend].index.values]
+
+    return proxDists, proxLocs
+
+
+if __name__ == '__main__':
+    proxDists, proxLocs = grabProxRecs()
--- treeRecDS.py
+++ treeRecDS.py
@@ -13,7 +13,8 @@ locationsDF = pd.read_csv(dataPath+'treeRecLocations.csv')
 distBetwRecsDF = pd.read_csv(dataPath+'distBetwRecs.csv')
 
 # data dimensions
-trials, directions = VmTreeDF.columns.levels[0].values, VmTreeDF.columns.levels[1].values
+trials = VmTreeDF.columns.levels[0].values
+directions = VmTreeDF.columns.levels[1].values
 tsteps, nrecs = VmTreeDF.shape[0], locationsDF.shape[0]
 numTrials, numDirs = len(trials), len(directions)
 print('num trials:', numTrials, ', directions:', directions)
@@ -22,6 +23,7 @@ print('num recs:', nrecs, ', time steps:', tsteps)
 # global params
 threshVm = -50
 
+
 def calcTreeDS(data, thresh):
     vals = data.values
     vals[vals < threshVm] = thresh
@@ -29,24 +31,25 @@ def calcTreeDS(data, thresh):
     area = np.sum((vals - thresh), axis=0).reshape(nrecs, numDirs, numTrials)
     print(area.shape)
 
-    xpts = area * np.cos(directions*np.pi/180).reshape(numDirs,1)
-    ypts = area * np.sin(directions*np.pi/180).reshape(numDirs,1)
+    xpts = area * np.cos(directions*np.pi/180).reshape(numDirs, 1)
+    ypts = area * np.sin(directions*np.pi/180).reshape(numDirs, 1)
     xsum = xpts.sum(axis=1)
     ysum = ypts.sum(axis=1)
 
     radius = np.sqrt(xsum**2 + ysum**2)
     DSi = radius / np.sum(area, axis=1)
-    theta = np.arctan2(ysum,xsum) * 180 / np.pi
+    theta = np.arctan2(ysum, xsum) * 180 / np.pi
 
     return theta, DSi
+
 
 def plotMap(locs, theta, DSi, degMax=180, dsiMax=1, trial=-1):
     locs = locs.values
 
     # calculate avgs and concatenate as new columns
-    theta = np.concatenate([theta, theta.mean(axis=1,keepdims=1)], axis=1)
-    theta = np.abs(theta) # linearize, diff from preffered
-    DSi = np.concatenate([DSi, DSi.mean(axis=1,keepdims=1)], axis=1)
+    theta = np.concatenate([theta, theta.mean(axis=1, keepdims=1)], axis=1)
+    theta = np.abs(theta)  # linearize, diff from preffered
+    DSi = np.concatenate([DSi, DSi.mean(axis=1, keepdims=1)], axis=1)
 
     # apply thresholds (colour range)
     theta[theta > degMax] = degMax
@@ -57,20 +60,22 @@ def plotMap(locs, theta, DSi, degMax=180, dsiMax=1, trial=-1):
 
     tFig, thetaMap = plt.subplots(1)
     thetaMap.scatter(
-        locs[:,0],locs[:,1], c=theta[:,trial],
+        locs[:, 0], locs[:, 1], c=theta[:, trial],
         cmap='jet', alpha=.5)
     thetaMap.set_title('Theta (vs Pref)')
     dFig, DSiMap = plt.subplots(1)
-    DSiMap.scatter(locs[:,0], locs[:,1], c=DSi[:,trial],
-        cmap='jet', alpha=.5)
+    DSiMap.scatter(locs[:, 0], locs[:, 1], c=DSi[:, trial],
+                   cmap='jet', alpha=.5)
     DSiMap.set_title('DSi')
 
     # create theta colour bar (legend)
-    sm = plt.cm.ScalarMappable(cmap='jet', norm=plt.Normalize(vmin=0, vmax=degMax))
+    sm = plt.cm.ScalarMappable(cmap='jet',
+                               norm=plt.Normalize(vmin=0, vmax=degMax))
     sm._A = []
     tFig.colorbar(sm)
     # create DSi colour bar (legend)
-    sm = plt.cm.ScalarMappable(cmap='jet', norm=plt.Normalize(vmin=0, vmax=dsiMax))
+    sm = plt.cm.ScalarMappable(cmap='jet',
+                               norm=plt.Normalize(vmin=0, vmax=dsiMax))
     sm._A = []
     dFig.colorbar(sm)
 
@@ -79,8 +84,10 @@ def plotMap(locs, theta, DSi, degMax=180, dsiMax=1, trial=-1):
 
     return tFig, dFig
 
+
 # create function for correlating activity between recs over the tree
-# create function for "line plots" of voltage along a rolled out section of dendrite
+# create function for "line plots" of voltage along a dendrite
+
 
 if __name__ == '__main__':
     theta, DSi = calcTreeDS(VmTreeDF, threshVm)
