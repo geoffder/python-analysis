6ed2077f72f4b2c5614adadb4fc1e0e76b96be81
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-08-20 17:09:00 -0700

    functioning encoder-decoder, but input/output shapes do not match. FIXIT
    Improved padding calculation is required (both downsampling and upsampling must be examined)

--- conv1d_autoencoder.py
+++ conv1d_autoencoder.py
@@ -18,34 +18,55 @@ def make_pool1d_layer(param_dict):
         return nn.AvgPool3d(*params)
 
 
+class Squeezer(nn.Module):
+    def __init__(self):
+        super(Squeezer, self).__init__()
+
+    @staticmethod
+    def forward(X):
+        return torch.squeeze(X)
+
+
+class Unsqueezer(nn.Module):
+    def __init__(self, dim):
+        super(Unsqueezer, self).__init__()
+        self.dim = dim
+
+    def forward(self, X):
+        return torch.unsqueeze(X, dim=self.dim)
+
+
 class Conv1dAutoEncoder(nn.Module):
 
-    def __init__(self, conv_params):
+    def __init__(self, params):
         super(Conv1dAutoEncoder, self).__init__()
-        self.conv_params = conv_params
+        self.params = params
         self.build()
 
     def build(self):
         encoder_mods = []
-
-        for p in self.conv_params:
-            k = p.get('kernel', 3)
-            pad = k // 2
-            encoder_mods.append(
-                nn.Conv1d(
-                    p['in'], p['out'], k, p.get('stride', 1), pad,
-                    p.get('dilation', 1), p.get('groups', 1),
-                    p.get('bias', True)
+        for p in self.params:
+            if p['type'] == 'conv':
+                k = p.get('kernel', 3)
+                pad = k // 2 if p.get('pad', 'same') != 'valid' else 0
+                encoder_mods.append(
+                    nn.Conv1d(
+                        p['in'], p['out'], k, p.get('stride', 1), pad,
+                        p.get('dilation', 1), p.get('groups', 1),
+                        p.get('bias', False)
+                    )
                 )
-            )
-            encoder_mods.append(nn.BatchNorm1d(p['out']))
-            encoder_mods.append(p.get('activation', nn.ReLU)())
-            if 'pool' in p:
-                encoder_mods.append(make_pool1d_layer(p['pool']))
-
-        # TODO: Add a flatten -> dense layer to do the reduction to 2 dims
-        # Easiest to do this with a seperate sequential, that way the
-        # corresponding decoder is a bit simpler...
+                encoder_mods.append(nn.BatchNorm1d(p['out']))
+                encoder_mods.append(p.get('activation', nn.ReLU)())
+                if 'pool' in p:
+                    encoder_mods.append(make_pool1d_layer(p['pool']))
+            elif p['type'] == 'dense':
+                encoder_mods.append(
+                    nn.Linear(p['in'], p['out'], p.get('bias', False))
+                )
+                encoder_mods.append(p.get('activation', nn.ReLU)())
+            elif p['type'] == 'squeeze':
+                encoder_mods.append(Squeezer())
 
         # package encoding layers as a Sequential network
         self.encoder_net = nn.Sequential(*encoder_mods)
@@ -54,14 +75,50 @@ class Conv1dAutoEncoder(nn.Module):
         return self.encoder_net(X)
 
     def decode(self, X):
-        # TODO: Add the dense dimension expansion -> reshaping
-        for layer in reversed(self.encoder_net, self.conv_params):
-            pad = layer.weight.shape[-1] // 2
-            X = F.conv_transpose1d(
-                X, layer.weight, layer.bias, layer.stride, pad, pad,
-                layer.groups, layer.dilation
-            )
+        for layer in reversed(self.encoder_net):
+            if isinstance(layer, nn.Conv1d):
+                pad = layer.weight.shape[-1] // 2
+                st_pad = layer.stride[0] // 2
+
+                X = F.conv_transpose1d(
+                    X, layer.weight, layer.bias, layer.stride, pad, st_pad,
+                    layer.groups, layer.dilation
+                )
+            elif isinstance(layer, nn.Linear):
+                X = F.linear(X, layer.weight.transpose(0, 1), layer.bias)
+            elif isinstance(layer, Squeezer):
+                X = torch.unsqueeze(X, dim=2)
+            # elif not isinstance(layer, nn.BatchNorm1d):
+            else:
+                X = layer(X)
         return X
 
     def forward(self, X):
+        # return self.encode(X)
         return self.decode(self.encode(X))
+
+
+def ae_build_1():
+    autoencoder = Conv1dAutoEncoder([
+        {'type': 'conv', 'in': 1, 'out': 128, 'kernel': 11, 'stride': 2},
+        {'type': 'conv', 'in': 128, 'out': 256, 'kernel': 11, 'stride': 2},
+        {'type': 'conv', 'in': 256, 'out': 512, 'kernel': 11, 'stride': 2},
+        {'type': 'conv', 'in': 512, 'out': 512, 'kernel': 5, 'stride': 2},
+        {'type': 'conv', 'in': 512, 'out': 256, 'kernel': 3, 'stride': 2},
+        {
+            'type': 'conv', 'in': 256, 'out': 128, 'kernel': 11, 'stride': 11,
+            # 'pad': 'valid'
+        },
+        {'type': 'squeeze'},
+        {'type': 'dense', 'in': 128, 'out': 2},
+    ])
+    return autoencoder
+
+
+if __name__ == '__main__':
+    autoencoder = ae_build_1()
+    a = torch.randn(10, 1, 350)
+    print(autoencoder.forward(a).shape)
+
+    # issues with padding calculations and stride are causing mismatch of
+    # downsampling -> transpose upsampling, so output is smaller than input.
