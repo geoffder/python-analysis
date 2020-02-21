18ef596381f8602cc9b7cb814a269f04f7bc6bd4
geoffder <geoffderosenroll@gmail.com>
2020-02-21 14:15:28 -0800

    Old un-commited changes and clean-up.

--- spikingDS.py
+++ spikingDS.py
@@ -2,27 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# load data
-base = 'C:\\Users\\geoff\\NEURONoutput\\'
-folder = 'spike_test\\'
-dataPath = base + folder
-
-spikesDF = pd.read_hdf(dataPath+'spikeData.h5', 'dirSpks')
-VmDF = pd.read_hdf(dataPath+'spikeData.h5', 'Vm')
-
-trials, directions = spikesDF.columns.values, spikesDF.index.values
-numTrials, numDirs = len(trials), len(directions)
-print('num trials:', numTrials, 'directions:', directions)
-
 
 def calcDS(data):
     vals = data.values
-    xpts = vals * np.cos(directions*np.pi/180).reshape(numDirs, 1)
-    ypts = vals * np.sin(directions*np.pi/180).reshape(numDirs, 1)
+    xpts = vals * np.cos(directions * np.pi / 180).reshape(numDirs, 1)
+    ypts = vals * np.sin(directions * np.pi / 180).reshape(numDirs, 1)
     xsum = xpts.sum(axis=0)
     ysum = ypts.sum(axis=0)
 
-    radius = np.sqrt(xsum**2 + ysum**2)
+    radius = np.sqrt(xsum ** 2 + ysum ** 2)
     DSi = radius / np.sum(vals, axis=0)
 
     theta = np.arctan2(ysum, xsum) * 180 / np.pi
@@ -38,7 +26,8 @@ def polarPlot(trials, directions, data, theta, DSi):
     inds = np.array(directions).argsort()
     circVals = vals[inds]
     circVals = np.concatenate(
-        [circVals, circVals[0, :].reshape(1, len(trials))], axis=0)
+        [circVals, circVals[0, :].reshape(1, len(trials))], axis=0
+    )
     circle = np.deg2rad([0, 45, 90, 135, 180, 225, 270, 315, 0])
 
     peak = np.max(vals)  # to set axis max
@@ -46,14 +35,13 @@ def polarPlot(trials, directions, data, theta, DSi):
     theta = np.deg2rad(theta)  # to radians for plotting
 
     fig = plt.figure()
-    polar = fig.add_subplot(111, projection='polar')
+    polar = fig.add_subplot(111, projection="polar")
     # plot trials lighter
-    polar.plot(circle, circVals, color='.75')
-    polar.plot([theta, theta], [np.zeros(len(trials)), DSi*peak], color='.75')
+    polar.plot(circle, circVals, color=".75")
+    polar.plot([theta, theta], [np.zeros(len(trials)), DSi * peak], color=".75")
     # plot avg darker
-    polar.plot(circle, np.mean(circVals, axis=1), color='.0', linewidth=2)
-    polar.plot([avgTheta, avgTheta], [0.0, avgDSi*peak],
-               color='.0', linewidth=2)
+    polar.plot(circle, np.mean(circVals, axis=1), color=".0", linewidth=2)
+    polar.plot([avgTheta, avgTheta], [0.0, avgDSi * peak], color=".0", linewidth=2)
     # misc settings
     polar.set_rlabel_position(-22.5)  # labels away from line
     polar.set_rmax(peak)
@@ -66,9 +54,21 @@ def polarPlot(trials, directions, data, theta, DSi):
     return fig
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # load data
+    base = "C:\\Users\\geoff\\NEURONoutput\\"
+    folder = "spike_test\\"
+    dataPath = base + folder
+
+    spikesDF = pd.read_hdf(dataPath + "spikeData.h5", "dirSpks")
+    VmDF = pd.read_hdf(dataPath + "spikeData.h5", "Vm")
+
+    trials, directions = spikesDF.columns.values, spikesDF.index.values
+    numTrials, numDirs = len(trials), len(directions)
+    print("num trials:", numTrials, "directions:", directions)
+
     theta, DSi = calcDS(spikesDF)
-    print('theta:', theta)
-    print('DSi:', DSi)
+    print("theta:", theta)
+    print("DSi:", DSi)
     fig = polarPlot(trials, directions, spikesDF, theta, DSi)
-    fig.savefig(dataPath+'polar.svg')
+    fig.savefig(dataPath + "polar.svg")
